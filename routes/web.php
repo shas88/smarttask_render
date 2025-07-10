bf26b1b646930563624e5c1e9aedeac94d4ce21a
Your Name <asthana.shashwat@gmail.com>
2025-07-10 20:37:10 +0530

    Add /check route for health test

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Task;
 
-
+Route::get('/check', fn() => 'Laravel is alive ✅');
 
 Route::get('/tasks-test', [TaskController::class, 'index']);
 
